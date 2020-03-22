152d0ea38df9e914bbf873b11e5b5f219c73f2d8
buttering <54806742+buttering@users.noreply.github.com>
2020-03-22 14:18:07 +0800

    Init MyPetStore

---
+++ src/main/java/org/csu/mypetstorespring/MypetstoreSpringApplication.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstorespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetstoreSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetstoreSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/csu/mypetstorespring/MypetstoreSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.mypetstorespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypetstoreSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
